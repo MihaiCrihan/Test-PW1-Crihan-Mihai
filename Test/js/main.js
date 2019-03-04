2edf97e5fe4c7c8c9154c72e0bdf089967591546
Mihai  Crihan <32567198+cronixm1@users.noreply.github.com>
2019-03-04 09:27:14 +0200

    Add files via upload

--- Test/js/main.js
+++ Test/js/main.js
@@ -54,8 +54,35 @@ console.log(worker2.getSalary());
 worker2.setTax(18);
 console.log(worker2.getSalaryNetto()); 
 
+
+
+
+
 if(worker1.getSalary() > worker2.getSalary()){
 	console.log(worker1.name + ' are mai mult ca ' + worker2.name + 'cu ' + (worker1.getSalary()-worker2.getSalary()));
 } else {
 	console.log(worker2.name + ' are mai mult ca ' + worker1.name + 'cu ' + (worker2.getSalary()-worker1.getSalary()));
 }
+console.log('=========================================================================================================================================================');
+var workers =[3];
+worker1 = new Worker('Mihai', 'Crihan', 24, 220); 
+worker2 = new Worker('Petru', 'Faranume', 8, 81); 
+worker3 = new Worker('Gabi', 'Emciuk', 10, 100);
+
+workers[0] = worker1;
+workers[1] = worker2;
+workers[2] = worker3;
+
+	for(let i = 0; i < 3; i++){
+		console.log(workers[i].getName()); 
+		console.log(workers[i].getSurname());
+		console.log(workers[i].getRate()); 
+		console.log(workers[i].getHours()); 
+		console.log(workers[i].getSalary()); 
+		workers[i].increaseSalary(2); 
+		console.log(workers[i].getSalary());
+		workers[i].setTax(18);
+		console.log(workers[i].getSalaryNetto()); 
+
+
+	}
